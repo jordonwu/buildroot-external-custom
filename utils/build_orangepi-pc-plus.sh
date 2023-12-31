a1522fcb0426be11327ad1e274852e902779c395
Ping Wu <pingwu@mail.ustc.edu.cn>
2023-12-31 15:20:32 +0800

    add --silent to reduce output log
    Signed-off-by: Ping Wu <pingwu@mail.ustc.edu.cn>

--- utils/build_orangepi-pc-plus.sh
+++ utils/build_orangepi-pc-plus.sh
@@ -2,4 +2,4 @@
 #
 make BR2_EXTERNAL=$PWD/buildroot-external-custom/ -C buildroot/  O=$PWD/output_orangepi-pc-plus custom_orangepi_pc_plus_defconfig
 cd $PWD/output_orangepi-pc-plus
-make -j8
+make -j8 --silent
