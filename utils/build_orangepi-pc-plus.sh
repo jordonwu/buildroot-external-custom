96f986a278dfab310c8886ef99038b8535b38ffa
Ping Wu <pingwu@mail.ustc.edu.cn>
2023-12-31 11:24:12 +0800

    add utils/build_orangepi-pc-plus.sh
    Signed-off-by: Ping Wu <pingwu@mail.ustc.edu.cn>

---
+++ utils/build_orangepi-pc-plus.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+make BR2_EXTERNAL=$PWD/buildroot-external-custom/ -C buildroot/  O=$PWD/output_orangepi-pc-plus custom_orangepi_pc_plus_defconfig
+cd $PWD/output_orangepi-pc-plus
+make -j8
