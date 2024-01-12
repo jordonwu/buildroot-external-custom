f69f03f18e62a73fc13cbc9296d013a3b2c81dac
Ping Wu <pingwu@mail.ustc.edu.cn>
2024-01-12 17:51:22 +0800

    add --silent to build
    Signed-off-by: Ping Wu <pingwu@mail.ustc.edu.cn>

--- utils/build_debix-model-a.sh
+++ utils/build_debix-model-a.sh
@@ -2,4 +2,4 @@
 #
 make BR2_EXTERNAL=$PWD/buildroot-external-custom/ -C buildroot/  O=$PWD/output_debix-model-a custom_polyhex_debix_model_a_defconfig
 cd $PWD/output_debix-model-a
-make -j8
+make -j8 --silent
